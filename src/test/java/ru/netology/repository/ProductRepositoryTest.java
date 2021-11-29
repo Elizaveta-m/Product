23dd61a9fe64bc2f620982c7b1d6aa3473bdc8ad
Elizaveta <rusquee@gmail.com>
2021-11-29 17:28:26 +0300

    NotFoundException added. Tests added

--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
 import org.junit.jupiter.api.Test;
@@ -9,13 +10,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductRepositoryTest {
     private ProductRepository repository = new ProductRepository();
     private Book coreJava = new Book(11, "core Java", 500, "author");
+    private Book second = new Book(2, "Sapiens", 700, "Harari");
+    private Book third = new Book(3, "Zuleikha", 500, "Yakhina");
+
+    @BeforeEach
+    public void setUp() {
+        repository.save(coreJava);
+        repository.save(second);
+        repository.save(third);
+    }
+
 
     @Test
-    public void shouldSaveOneItem() {
-        repository.save(coreJava);
-
-        Product[] expected = new Product[]{coreJava};
+    void shouldRemoveById() {
+        repository.removeById(11);
+        Product[] expected = new Product[]{second, third};
         Product[] actual = repository.findAll();
         assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldThrowException() {
+        assertThrows(NotFoundException.class, () -> repository.removeById(999));
+
     }
 }
